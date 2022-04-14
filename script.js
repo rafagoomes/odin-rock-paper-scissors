3efddaf0c9089e02e9524d7d6130cd0237103a9d
rafagoomes <rafael.goomes91@gmail.com>
2022-04-14 09:30:37 -0300

    Entire rock-paper-scissors game

---
+++ script.js
@@ -0,0 +1,43 @@
+//Computer Play
+function computerPlay() {
+    var options = ['rock', 'paper', 'scissors'];
+    const selectedOption = Math.floor(Math.random() * options.length);
+    //console.log(options[selectedOption]);
+    return options[selectedOption];
+}
+//Play round
+function playRound(computerSelection, playerSelection) {
+    playerSelection.toLowerCase();
+
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You lose! Paper beats rock."
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You win! Paper beats rock."
+    } else if (playerSelection === 'rock' && computerSelection === 'rock'){
+        return `Draw - ${playerSelection} x ${computerSelection}`;
+    }
+    if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return "You lose! Rock beats scissors."
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return "You win! Rock beats scissors."
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
+        return `Draw - ${playerSelection} x ${computerSelection}`;
+    } 
+    if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return "You lose! Scissor beats paper."
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You win! Scissor beats paper."
+    } else if (playerSelection === 'paper' && computerSelection === 'paper'){
+        return `Draw - ${playerSelection} x ${computerSelection}`;
+    }
+
+}
+//Play game of 5 rounds
+function game() {
+    for (i = 1; i <= 5; i++) {
+        const playerSelection = prompt("Type one of the options - Scissors, Paper or Rock;");
+        console.log(playRound(computerPlay(), playerSelection.toLowerCase()));
+    }
+}
+
+game();
