a4782859e22745014c2fcf209df5c0d989218565
rafagoomes <rafael.goomes91@gmail.com>
2022-04-14 09:53:26 -0300

    Removed validation for now

--- script.js
+++ script.js
@@ -7,31 +7,26 @@ function computerPlay() {
 }
 //Play round
 function playRound(computerSelection, playerSelection) {
-
-    if (playerSelection === '' || playerSelection !== options) {
-        return `Wrong selection, please select any of ${options}`;
-    } else {
-        if (playerSelection === 'rock' && computerSelection === 'paper') {
-            return "You lose! Paper beats rock."
-        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            return "You win! Paper beats rock."
-        } else if (playerSelection === 'rock' && computerSelection === 'rock'){
-            return `Draw - ${playerSelection} x ${computerSelection}`;
-        }
-        if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            return "You lose! Rock beats scissors."
-        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            return "You win! Rock beats scissors."
-        } else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
-            return `Draw - ${playerSelection} x ${computerSelection}`;
-        } 
-        if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            return "You lose! Scissor beats paper."
-        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            return "You win! Scissor beats paper."
-        } else if (playerSelection === 'paper' && computerSelection === 'paper'){
-            return `Draw - ${playerSelection} x ${computerSelection}`;
-        }
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You lose! Paper beats rock."
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You win! Paper beats rock."
+    } else if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return `Draw - ${playerSelection} x ${computerSelection}`;
+    }
+    if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return "You lose! Rock beats scissors."
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return "You win! Rock beats scissors."
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return `Draw - ${playerSelection} x ${computerSelection}`;
+    }
+    if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return "You lose! Scissor beats paper."
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You win! Scissor beats paper."
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return `Draw - ${playerSelection} x ${computerSelection}`;
     }
 }
 //Play game of 5 rounds
